8042d242b3c05e7dc21e4864e3e527b79ea86b5d
Agraw Mindaye <agraw@Aminats-MacBook-Pro.local>
2025-09-25 15:41:04 -0400

    project cleanup and setup

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+// About Content
+
+export default function AboutContent() {
+    return (
+      <section>
+        <h1>About Stargaze</h1>
+        <p>This app helps you discover and track movies</p>
+      </section>
+    )
+  }
+  
